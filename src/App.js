e1e288542b1948dafd84d4c29bfc90333142a3d0
rapelliswathi <rapelliswathi1122@gmail.com>
2024-03-11 08:41:55 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import BlogItemDetails from './components/BlogItemDetails'
+import Header from './components/Header'
+import About from './components/About'
+import Home from './components/Home'
+import Contact from './components/Contact'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <div className="app-container">
+    <div className="responsive-container">
+      <BrowserRouter>
+        <Header />
+        <div className="app-body">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route path="/blogs/:id" component={BlogItemDetails} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  </div>
+)
+
+export default App
